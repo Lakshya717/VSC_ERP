761f80f54b45105e9f9f1f411f33afad1d79ccf9
Lakshya Dubey <Lakshya_717@outlook.com>
2025-10-23 01:37:39 +0530

    finished linking urls; Service App is the homebase with index

--- VSC_ERP/settings.py
+++ VSC_ERP/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'VSC_ERP.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,7 +123,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
@@ -134,6 +134,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- VSC_ERP/urls.py
+++ VSC_ERP/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('Masters/',include('Masters.urls')),
+
+    path('Customer/',include('Customer.urls')),
+    path('Employee/',include('Employee.urls')),
+    path('Inventory/',include('Inventory.urls')),
+    path('Service/',include('Service.urls')),
+    path('Vehicle/',include('Vehicle.urls')),
 ]
